74f84dfcb6d5700413a047a4f263df1564cf4e57
Soumabha <soumabha.chaudhuri@quikr.com>
2016-08-18 18:32:11 +0530

    Changes to Gulpfile and addition of dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var gutil = require('gulp-util');
+var concatCss = require('gulp-concat-css');
 
 // Lint Task
 gulp.task('lint', function() {
@@ -15,11 +16,17 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter('default'));
 });
 
+// Concatenate css
+gulp.task('cssconcat', function(){
+	return gulp.src('src/css/*.css')
+		.pipe(concatCss('fholix.css'))
+		.pipe(gulp.dest('dist/css'));
+})
+
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
     return gulp.src('src/js/*.js')
         .pipe(concat('fholix.js'))
-        .pipe(gulp.dest('dist'))
         .pipe(rename('fholix.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('dist/js'));
