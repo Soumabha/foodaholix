a4cc8ce7d1e65077c0c2e0b69b96ce39acabb819
Soumabha <soumabha.chaudhuri@quikr.com>
2016-08-18 16:19:35 +0530

    Refactoring and using gulp

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var gutil = require('gulp-util');
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('src/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('src/js/*.js')
+        .pipe(concat('fholix.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('fholix.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('src/js/*.js', ['lint', 'scripts']);
+});
+
+// Default Task
+gulp.task('default', ['scripts', 'watch']);
