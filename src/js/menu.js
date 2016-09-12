a47c6a4415de156484c35ee7ffe65b2a0604704c
Soumabha <soumabha.chaudhuri@quikr.com>
2016-09-13 00:43:06 +0530

    Slider changes

--- src/js/menu.js
+++ src/js/menu.js
@@ -106,6 +106,19 @@ var data = [
    }
 ];
 
+var sliderData = [
+   {
+      imageLink: "//stage.foodaholixassets.s3.amazonaws.com/images/SliderChefArifMiyankiRasoi.jpg",
+      header: "Chef Arif Miyan ki Rasoi : Marriott, Gurgaon",
+      text: "The Gosht Nihari is a dum pukt stew along with the bone marrow was quite different from the one available in Delhi. Chef Arif knows how to work his meat and bring out subtle flavours in them."
+   },
+   {
+      imageLink: "//stage.foodaholixassets.s3.amazonaws.com/images/SliderTAB.jpg",
+      header: "The Ancient Barbeque Gurgaon Open Grill ",
+      text: "Keema Tart is a must try for all meat lovers. It is a sweet crispy tart with keema filling."
+   }
+];
+
 var constants = {
     arrowDown: "",
 	arrowRight: ""
@@ -245,3 +258,35 @@ function init () {
 }
 
 init();
+
+function createSlider() {
+  var sliderInner = document.getElementById("slider-inner");
+
+  var navDots = document.getElementById("nav-dots");
+
+  for(var i=0, len = sliderData.length; i<len; i++) {
+    var slideItem = sliderData[i];
+    var data_orientation = "vertical";
+    var navSpan = "<span>";
+    if(i === 0 ){
+      navSpan = "<span class='nav-dot-current'>";
+      data_orientation="horizontal";
+    }
+
+    var htmlString = "<div class='sl-slide' data-orientation='"+data_orientation+"' data-slice1-rotation='-25' data-slice2-rotation='-25' data-slice1-scale='2' data-slice2-scale='2' style='display: block;'><div class='sl-slide-inner'>";
+    htmlString += "<div class='bg-img' style='background-image:url("+ slideItem.imageLink +"); background-size: 1046px 600px'></div>";
+    
+    if(slideItem.header) {
+      htmlString+= "<h2>" + slideItem.header + "</h2>";
+    }
+
+    if(slideItem.text) {
+      htmlString+= "<blockquote><p>" + slideItem.text + "</p></blockquote>";
+    }
+    sliderInner.innerHTML += htmlString + "</div></div>";
+
+    navDots.innerHTML = navDots.innerHTML + navSpan + "</span>";
+  }
+}
+
+createSlider();
